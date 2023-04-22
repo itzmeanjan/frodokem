5f2fa6b23e135d5c211bef4cd8ed9ef480d02664
Anjan Roy <hello@itzmeanjan.in>
2023-04-22 20:57:11 +0530

    ensure encoding of bit string to matrix and vice versa (algo 1, 2 of spec) is implemented correctly
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include "test/test_encoding.hpp"
 #include "test/test_frodo.hpp"
 #include <iostream>
 
@@ -11,6 +12,14 @@ main()
   test_frodo::test_zq_encode_decode<1u << 16, 3>();
   test_frodo::test_zq_encode_decode<1u << 16, 4>();
   std::cout << "[test] Encoding/ decoding of Zq elements\n";
+
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 2>();
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 3>();
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 4>();
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 2>();
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 3>();
+  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 4>();
+  std::cout << "[test] Encoding/ decoding of matrix over Zq\n";
 
   return 0;
 }
