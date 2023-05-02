b34d290064c563b4908fbc5ffa05aaf2ad4ec6b7
Anjan Roy <hello@itzmeanjan.in>
2023-05-02 21:31:41 +0530

    compile-time computable byte length of frodoPKE public/secret key
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -16,4 +16,26 @@ inline constexpr size_t log2(const T v)
   return std::countr_zero(v);
 }
 
+// Compile-time computable byte length of Frodo PKE public key.
+constexpr size_t
+pke_pub_key_len(const size_t n,
+                const size_t n_bar,
+                const size_t len_seed_A,
+                const uint32_t Q)
+{
+  const size_t bit_len = len_seed_A +           // bit length of seed
+                         (n * n_bar * log2(Q)); // matrix B packed as bitstring
+  const size_t byte_len = (bit_len + 7) / 8;
+  return byte_len;
+}
+
+// Compile-time computable byte length of Frodo PKE secret key.
+constexpr size_t
+pke_sec_key_len(const size_t n_bar, const size_t n, const uint32_t Q)
+{
+  const size_t bit_len = n_bar * n * log2(Q);
+  const size_t byte_len = (bit_len + 7) / 8;
+  return byte_len;
+}
+
 }
