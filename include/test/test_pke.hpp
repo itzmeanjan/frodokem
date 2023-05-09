340fe209ffd6440e98de11ac3c08201b257c4f04
Anjan Roy <hello@itzmeanjan.in>
2023-05-09 09:02:04 +0400

    small refactoring to make Frodo PKE testing code look better
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_pke.hpp
+++ include/test/test_pke.hpp
@@ -18,9 +18,9 @@ template<const size_t n,
          const size_t l,
          const size_t m_bar,
          const size_t n_bar,
-         const size_t len_seed_A,
-         const size_t len_seed_SE,
-         const size_t len_χ,
+         const size_t lA,
+         const size_t lSE,
+         const size_t lχ,
          const uint32_t Q,
          const size_t B>
 void
@@ -29,12 +29,12 @@ test_pke()
   namespace utils = frodo_utils;
 
   constexpr size_t mlen = l / 8;
-  constexpr size_t pklen = utils::pke_pub_key_len(n, n_bar, len_seed_A, Q);
+  constexpr size_t pklen = utils::pke_pub_key_len(n, n_bar, lA, Q);
   constexpr size_t sklen = utils::pke_sec_key_len(n, n_bar, Q);
   constexpr size_t ctlen = utils::pke_cipher_text_len(n, m_bar, n_bar, Q);
 
-  auto seedA = static_cast<uint8_t*>(std::malloc(len_seed_A / 8));
-  auto seedSE = static_cast<uint8_t*>(std::malloc(len_seed_SE / 8));
+  auto seedA = static_cast<uint8_t*>(std::malloc(lA / 8));
+  auto seedSE = static_cast<uint8_t*>(std::malloc(lSE / 8));
   auto pkey = static_cast<uint8_t*>(std::malloc(pklen));
   auto skey = static_cast<uint8_t*>(std::malloc(sklen));
   auto msg = static_cast<uint8_t*>(std::malloc(mlen));
@@ -43,15 +43,17 @@ test_pke()
 
   prng::prng_t prng;
 
-  prng.read(seedA, len_seed_A / 8);
-  prng.read(seedSE, len_seed_SE / 8);
+  prng.read(seedA, lA / 8);
+  prng.read(seedSE, lSE / 8);
   prng.read(msg, mlen);
 
-  pke::keygen<n, n_bar, len_seed_A, len_seed_SE, len_χ, Q, B>(
-    seedA, seedSE, pkey, skey);
-  pke::encrypt<n, l, m_bar, n_bar, len_seed_A, len_seed_SE, len_χ, Q, B>(
-    seedSE, pkey, msg, cipher);
-  pke::decrypt<n, l, m_bar, n_bar, Q, B>(skey, cipher, decrypted);
+  {
+    using namespace pke;
+
+    keygen<n, n_bar, lA, lSE, lχ, Q, B>(seedA, seedSE, pkey, skey);
+    encrypt<n, l, m_bar, n_bar, lA, lSE, lχ, Q, B>(seedSE, pkey, msg, cipher);
+    decrypt<n, l, m_bar, n_bar, Q, B>(skey, cipher, decrypted);
+  }
 
   for (size_t i = 0; i < mlen; i++) {
     assert(msg[i] == decrypted[i]);
