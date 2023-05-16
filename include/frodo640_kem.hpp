c5ce4cd21f83e7c3baca9b66d28921bab029dda7
Anjan Roy <hello@itzmeanjan.in>
2023-05-16 17:31:06 +0400

    instantiate frodo-640 KEM encapsulation routine
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodo640_kem.hpp
+++ include/frodo640_kem.hpp
@@ -27,6 +27,9 @@ constexpr auto PUB_KEY_LEN = kem::kem_pub_key_len(n, n̄, lA, Q);
 // = 19248 -bytes secret key
 constexpr auto SEC_KEY_LEN = kem::kem_sec_key_len(n, n̄, ls, lA, lpkh, Q);
 
+// = 9720 -bytes cipher text
+constexpr auto CIPHER_LEN = kem::kem_cipher_text_len(n, m̄, n̄, Q);
+
 // Given 16 -bytes seed s ( secret part of private key ), 16 -bytes seed seedSE
 // ( used for sampling error matrices ) and 16 -bytes seed z ( used for deriving
 // pseudo-random seed seedA, which is used for generating matrix A ), this
@@ -40,6 +43,19 @@ keygen(std::span<const uint8_t, ls / 8> s,
        std::span<uint8_t, SEC_KEY_LEN> skey)
 {
   kem::keygen<n, n̄, lA, lSE, ls, lz, lpkh, lχ, Q, B>(s, seedSE, z, pkey, skey);
+}
+
+// Given a 16 -bytes key μ ( which is actually encrypted using underlying PKE
+// scheme ) and a Frodo-640 KEM public key, this routine can be used for
+// computing a cipher text ( which can only be decrypted using corresponding
+// Frodo-640 KEM private key ) and a 16 -bytes shared secret.
+inline void
+encaps(std::span<const uint8_t, (lμ + 7) / 8> μ,
+       std::span<const uint8_t, PUB_KEY_LEN> pkey,
+       std::span<uint8_t, CIPHER_LEN> enc,
+       std::array<uint8_t, (lss + 7) / 8> ss)
+{
+  kem::encaps<n, m̄, n̄, lA, lSE, lss, lk, lμ, lpkh, lχ, Q, B>(μ, pkey, enc, ss);
 }
 
 }
