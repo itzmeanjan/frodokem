9546b00689c92f3256d0c50e2d210732aa9f8fa0
Anjan Roy <hello@itzmeanjan.in>
2023-05-21 15:05:56 +0400

    refactor arithmetic over Zq s.t. D is a template parameter | q = 1<<D
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/zq.hpp
+++ include/zq.hpp
@@ -5,23 +5,23 @@
 #include <cstddef>
 #include <cstdint>
 
-// Arithmetic over Zq | q ∈ {2^15, 2^16}
+// Arithmetic over Zq | q = 2^D and D <= 16
 namespace zq {
 
-// Wrapper type encapsulating arithmetic over Zq s.t. q is a power of 2 value.
-template<const uint32_t Q>
-  requires(frodo_params::check_q(Q))
+// Wrapper type encapsulating arithmetic over Zq i.e. mod q (= 2^D).
+template<const size_t D>
+  requires(frodo_params::check_d(D))
 struct zq_t
 {
 private:
-  uint32_t v = 0u;
+  uint16_t v = 0u;
 
 public:
   // Default constructor returning Zq, with value 0.
   inline constexpr zq_t() = default;
 
-  // Given an unsigned 32 -bit integer, it constructs an element ∈ Zq
-  inline constexpr zq_t(const uint32_t a) { this->v = a % Q; }
+  // Given an unsigned 16 -bit integer, it constructs an element ∈ Zq
+  inline constexpr zq_t(const uint16_t _v) { this->v = _v; }
 
   // Addition of two integers modulo Q
   inline constexpr zq_t operator+(const zq_t& rhs) const
@@ -47,13 +47,13 @@ public:
     return zq_t(this->v * rhs.v);
   }
 
-  // Check equality of two Zq elements
+  // Check equality between canonical form of two Zq elements
   inline constexpr bool operator==(const zq_t& rhs) const
   {
-    return this->v == rhs.v;
+    return this->to_canonical() == rhs.to_canonical();
   }
 
-  // Check inequality of two Zq elements
+  // Check inequality between canonical form of two Zq elements
   inline constexpr bool operator!=(const zq_t& rhs) const
   {
     return !(*this == rhs);
@@ -63,14 +63,13 @@ public:
   // s.t. q = 2^D and B <= D, following definition of `ec(k)` function, in
   // section 2.2.1 of FrodoKEM specification.
   template<const size_t B>
-  static inline constexpr zq_t encode(const uint32_t k)
-    requires(B <= frodo_utils::log2(Q))
+  static inline constexpr zq_t encode(const uint16_t k)
+    requires(B <= D)
   {
-    constexpr size_t D = frodo_utils::log2(Q);
-    constexpr uint32_t mask = (1u << B) - 1u;
+    constexpr uint16_t mask = (1u << B) - 1u;
     constexpr size_t shl = D - B;
 
-    const uint32_t v = (k & mask) << shl;
+    const uint16_t v = (k & mask) << shl;
     return zq_t(v);
   }
 
@@ -78,33 +77,35 @@ public:
   // s.t. returned integer v ∈ [0, 2^B), collecting inspiration from
   // https://github.com/microsoft/PQCrypto-LWEKE/blob/d7037ccb/python3/frodokem.py#L335.
   template<const size_t B>
-  inline constexpr uint32_t decode() const
-    requires(B <= frodo_utils::log2(Q))
+  inline constexpr uint16_t decode() const
+    requires(B <= D)
   {
-    constexpr size_t D = frodo_utils::log2(Q);
     constexpr uint32_t mask = (1u << B) - 1u;
 
-    const uint32_t v = (((this->v << B) + (1u << (D - 1))) >> D) & mask;
-    return v;
+    const uint32_t t = (static_cast<uint32_t>(this->v) << B) + (1u << (D - 1));
+    const uint32_t v = (t >> D) & mask;
+    return static_cast<uint16_t>(v);
   }
 
-  // Get the raw value behind Zq wrapper type.
-  inline constexpr uint32_t get_value() const { return this->v; }
+  // Get the canonical value ( i.e. reduced by Q ) behind Zq wrapper type.
+  inline constexpr uint16_t to_canonical() const
+  {
+    constexpr uint16_t mask = (1u << D) - 1u;
+    return this->v & mask;
+  }
 
-  // Reads four random bytes from PRNG and computes a random element ∈ Zq.
+  // Get the raw 16 -bit value behind Zq wrapper type.
+  inline constexpr uint16_t to_raw() const { return this->v; }
+
+  // Reads two random bytes from PRNG and computes a random element ∈ Zq.
   static inline zq_t random_value(prng::prng_t& prng)
   {
-    uint32_t res = 0;
+    uint16_t res = 0;
+    // note, no specific endianness is preferred here !
     prng.read(reinterpret_cast<uint8_t*>(&res), sizeof(res));
 
     return zq_t(res);
   }
-
-  // Writes an element of Zq to output stream
-  friend std::ostream& operator<<(std::ostream& os, const zq_t& elm)
-  {
-    return os << "Zq(" << elm.v << ", " << Q << ")";
-  };
 };
 
 }
