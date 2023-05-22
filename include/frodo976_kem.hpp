45347ca8cf15e3d0841c8172600492cb15031781
Anjan Roy <hello@itzmeanjan.in>
2023-05-22 15:10:32 +0400

    update instantiations of FrodoKEM routines
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/frodo976_kem.hpp
+++ include/frodo976_kem.hpp
@@ -6,7 +6,6 @@ namespace frodo976_kem {
 
 // See column 1, 3 of table 4 of FrodoKEM specification.
 constexpr size_t D = 16;
-constexpr size_t Q = 1u << D;
 constexpr size_t n = 976;
 constexpr size_t m̄ = 8;
 constexpr size_t n̄ = 8;
@@ -22,13 +21,13 @@ constexpr size_t lss = 192;  // = len_ss
 constexpr size_t lχ = 16;    // = len_χ
 
 // = 15632 -bytes public key
-constexpr auto PUB_KEY_LEN = kem::kem_pub_key_len(n, n̄, lA, Q);
+constexpr auto PUB_KEY_LEN = kem::kem_pub_key_len(n, n̄, lA, D);
 
 // = 31296 -bytes secret key
-constexpr auto SEC_KEY_LEN = kem::kem_sec_key_len(n, n̄, ls, lA, lpkh, Q);
+constexpr auto SEC_KEY_LEN = kem::kem_sec_key_len(n, n̄, ls, lA, lpkh, D);
 
 // = 15744 -bytes cipher text
-constexpr auto CIPHER_LEN = kem::kem_cipher_text_len(n, m̄, n̄, Q);
+constexpr auto CIPHER_LEN = kem::kem_cipher_text_len(n, m̄, n̄, D);
 
 // Given 24 -bytes seed s ( secret part of private key ), 24 -bytes seed seedSE
 // ( used for sampling error matrices ) and 16 -bytes seed z ( used for deriving
@@ -42,7 +41,7 @@ keygen(std::span<const uint8_t, ls / 8> s,
        std::span<uint8_t, PUB_KEY_LEN> pkey,
        std::span<uint8_t, SEC_KEY_LEN> skey)
 {
-  kem::keygen<n, n̄, lA, lSE, ls, lz, lpkh, lχ, Q, B>(s, seedSE, z, pkey, skey);
+  kem::keygen<n, n̄, lA, lSE, ls, lz, lpkh, lχ, D, B>(s, seedSE, z, pkey, skey);
 }
 
 // Given a 24 -bytes key μ ( which is actually encrypted using underlying PKE
@@ -55,7 +54,7 @@ encaps(std::span<const uint8_t, (lμ + 7) / 8> μ,
        std::span<uint8_t, CIPHER_LEN> enc,
        std::span<uint8_t, (lss + 7) / 8> ss)
 {
-  kem::encaps<n, m̄, n̄, lA, lSE, lss, lk, lμ, lpkh, lχ, Q, B>(μ, pkey, enc, ss);
+  kem::encaps<n, m̄, n̄, lA, lSE, lss, lk, lμ, lpkh, lχ, D, B>(μ, pkey, enc, ss);
 }
 
 // Given Frodo-976 KEM secret key, which is associated with the public key,
@@ -67,7 +66,7 @@ decaps(std::span<const uint8_t, SEC_KEY_LEN> skey,
        std::span<const uint8_t, CIPHER_LEN> enc,
        std::span<uint8_t, (lss + 7) / 8> ss)
 {
-  kem::decaps<n, m̄, n̄, lA, lSE, ls, lss, lk, lμ, lpkh, lχ, Q, B>(skey, enc, ss);
+  kem::decaps<n, m̄, n̄, lA, lSE, ls, lss, lk, lμ, lpkh, lχ, D, B>(skey, enc, ss);
 }
 
 }
