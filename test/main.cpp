80305c32c3a38626fb948f62f47bf3e94c9c78fc
Anjan Roy <hello@itzmeanjan.in>
2023-05-22 15:28:01 +0400

    enable all test cases ensuring correctness and conformance of FrodoKEM
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- test/main.cpp
+++ test/main.cpp
@@ -4,91 +4,51 @@
 int
 main()
 {
-  test_frodo::test_zq_encode_decode<1u << 15, 2>();
-  test_frodo::test_zq_encode_decode<1u << 16, 3>();
-  test_frodo::test_zq_encode_decode<1u << 16, 4>();
-  test_frodo::test_lemma_2_18<1u << 15, 2>();
-  test_frodo::test_lemma_2_18<1u << 16, 3>();
-  test_frodo::test_lemma_2_18<1u << 16, 4>();
+  test_frodo::test_zq_encode_decode<15, 2>();
+  test_frodo::test_zq_encode_decode<16, 3>();
+  test_frodo::test_zq_encode_decode<16, 4>();
+  test_frodo::test_lemma_2_18<15, 2>();
+  test_frodo::test_lemma_2_18<16, 3>();
+  test_frodo::test_lemma_2_18<16, 4>();
   std::cout << "[test] Encoding/ decoding of Zq elements\n";
 
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 2>();
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 3>();
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 15, 4>();
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 2>();
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 3>();
-  test_frodo::test_matrix_encode_decode<8, 8, 1u << 16, 4>();
+  test_frodo::test_matrix_encode_decode<8, 8, 15, 2>();
+  test_frodo::test_matrix_encode_decode<8, 8, 15, 3>();
+  test_frodo::test_matrix_encode_decode<8, 8, 15, 4>();
+  test_frodo::test_matrix_encode_decode<8, 8, 16, 2>();
+  test_frodo::test_matrix_encode_decode<8, 8, 16, 3>();
+  test_frodo::test_matrix_encode_decode<8, 8, 16, 4>();
   std::cout << "[test] Encoding/ decoding of matrix over Zq\n";
 
-  test_frodo::test_matrix_pack_unpack<640, 8, 1u << 15>();
-  test_frodo::test_matrix_pack_unpack<8, 640, 1u << 15>();
-  test_frodo::test_matrix_pack_unpack<976, 8, 1u << 16>();
-  test_frodo::test_matrix_pack_unpack<8, 976, 1u << 16>();
-  test_frodo::test_matrix_pack_unpack<1344, 8, 1u << 16>();
-  test_frodo::test_matrix_pack_unpack<8, 1344, 1u << 16>();
-  test_frodo::test_matrix_pack_unpack<8, 8, 1u << 15>();
-  test_frodo::test_matrix_pack_unpack<8, 8, 1u << 16>();
+  test_frodo::test_matrix_pack_unpack<640, 8, 15>();
+  test_frodo::test_matrix_pack_unpack<8, 640, 15>();
+  test_frodo::test_matrix_pack_unpack<976, 8, 16>();
+  test_frodo::test_matrix_pack_unpack<8, 976, 16>();
+  test_frodo::test_matrix_pack_unpack<1344, 8, 16>();
+  test_frodo::test_matrix_pack_unpack<8, 1344, 16>();
+  test_frodo::test_matrix_pack_unpack<8, 8, 15>();
+  test_frodo::test_matrix_pack_unpack<8, 8, 16>();
   std::cout << "[test] Packing/ unpacking of matrix over Zq\n";
 
-  test_frodo::test_matrix_transpose<8, 640, 1u << 15>();
-  test_frodo::test_matrix_transpose<8, 976, 1u << 16>();
-  test_frodo::test_matrix_transpose<8, 1344, 1u << 16>();
+  test_frodo::test_matrix_transpose<8, 640, 15>();
+  test_frodo::test_matrix_transpose<8, 976, 16>();
+  test_frodo::test_matrix_transpose<8, 1344, 16>();
 
-  test_frodo::test_matrix_add_sub<8, 640, 1u << 15>();
-  test_frodo::test_matrix_add_sub<8, 976, 1u << 16>();
-  test_frodo::test_matrix_add_sub<8, 1344, 1u << 16>();
-  test_frodo::test_matrix_add_sub<8, 8, 1u << 15>();
-  test_frodo::test_matrix_add_sub<8, 8, 1u << 16>();
+  test_frodo::test_matrix_add_sub<8, 640, 15>();
+  test_frodo::test_matrix_add_sub<8, 976, 16>();
+  test_frodo::test_matrix_add_sub<8, 1344, 16>();
+  test_frodo::test_matrix_add_sub<8, 8, 15>();
+  test_frodo::test_matrix_add_sub<8, 8, 16>();
   std::cout << "[test] Operations on matrices over Zq\n";
 
-  test_frodo::test_pke<640, 128, 8, 8, 128, 128, 16, 1u << 15, 2>();
-  test_frodo::test_pke<976, 192, 8, 8, 128, 192, 16, 1u << 16, 3>();
-  test_frodo::test_pke<1344, 256, 8, 8, 128, 256, 16, 1u << 16, 4>();
-  std::cout << "[test] Frodo Public Key Encryption\n";
+  {
+    using namespace test_frodo;
 
-  test_frodo::test_kem<640,
-                       8,
-                       8,
-                       128,
-                       128,
-                       128,
-                       128,
-                       128,
-                       128,
-                       128,
-                       128,
-                       16,
-                       1u << 15,
-                       2>();
-  test_frodo::test_kem<976,
-                       8,
-                       8,
-                       128,
-                       192,
-                       192,
-                       128,
-                       192,
-                       192,
-                       192,
-                       192,
-                       16,
-                       1u << 16,
-                       3>();
-  test_frodo::test_kem<1344,
-                       8,
-                       8,
-                       128,
-                       256,
-                       256,
-                       128,
-                       256,
-                       256,
-                       256,
-                       256,
-                       16,
-                       1u << 16,
-                       4>();
-  std::cout << "[test] Frodo Key Encapsulation Mechanism\n";
+    test_kem<640, 8, 8, 128, 128, 128, 128, 128, 128, 128, 128, 16, 15, 2>();
+    test_kem<976, 8, 8, 128, 192, 192, 128, 192, 192, 192, 192, 16, 16, 3>();
+    test_kem<1344, 8, 8, 128, 256, 256, 128, 256, 256, 256, 256, 16, 16, 4>();
+    std::cout << "[test] Frodo Key Encapsulation Mechanism\n";
+  }
 
   test_frodo::test_frodo640_kem_kat();
   test_frodo::test_frodo976_kem_kat();
