c330e2c247ed995aa21104baa3b6ae8b769df48a
Anjan Roy <hello@itzmeanjan.in>
2023-05-22 15:44:33 +0400

    remove unnecessary function for computing logarithm base 2
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -13,15 +13,6 @@
 // Some utility functions, required for FrodoKEM
 namespace frodo_utils {
 
-// Given an unsigned integer of bitwidth b ∈ {8, 16, 32, 64}, this routine can
-// be used for computing logarithm base 2 for power of 2 value s.t. v ∈ [1, 2^b)
-template<typename T>
-  requires(std::is_unsigned_v<T>)
-inline constexpr size_t log2(const T v)
-{
-  return std::countr_zero(v);
-}
-
 // Compile-time computable byte length of Frodo KEM public key.
 constexpr size_t
 kem_pub_key_len(const size_t n,
