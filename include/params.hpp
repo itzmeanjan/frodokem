a1dd2b7d71645abe44ddb9abe22d201e49ded9af
Anjan Roy <hello@itzmeanjan.in>
2023-05-25 11:24:52 +0400

    update compile-time executable functions - template parameter checkers, using C++ `<concepts>`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/params.hpp
+++ include/params.hpp
@@ -21,101 +21,81 @@ check_b(const size_t b)
   return (b == 2) || (b == 3) || (b == 4);
 }
 
-// Compile-time executable check for ensuring that FrodoKEM's parameter len_χ
-// only takes arguments suggested on table 4 of FrodoKEM specification.
-constexpr bool
-check_len_χ(const size_t len_χ)
-{
-  return len_χ == 16;
-}
-
 // Compile-time executable check for ensuring that FrodoKEM key generation
-// routine is invoked with proper arguments, as suggested on table 4 of FrodoKEM
-// specification.
+// routine is invoked with proper arguments, as suggested on table A.1, A.2 of
+// FrodoKEM specification.
 constexpr bool
-check_frodo_keygen_params(const size_t n,
-                          const size_t n_bar,
-                          const size_t len_seed_A,
-                          const size_t len_seed_SE,
-                          const size_t len_s,
-                          const size_t len_z,
-                          const size_t len_pkh,
-                          const size_t len_χ,
-                          const size_t D,
-                          const size_t B)
+check_keygen_params(const size_t n,
+                    const size_t n̄,
+                    const size_t len_sec,
+                    const size_t len_SE,
+                    const size_t len_A,
+                    const size_t B,
+                    const size_t D)
 {
-  return ((n == 640) && (n_bar == 8) && (len_seed_A == 128) &&
-          (len_seed_SE == 128) && (len_s == 128) && (len_z == 128) &&
-          (len_pkh == 128) && (len_χ == 16) && (D == 15) && (B == 2)) ||
-         ((n == 976) && (n_bar == 8) && (len_seed_A == 128) &&
-          (len_seed_SE == 192) && (len_s == 192) && (len_z == 128) &&
-          (len_pkh == 192) && (len_χ == 16) && (D == 16) && (B == 3)) ||
-         ((n == 1344) && (n_bar == 8) && (len_seed_A == 128) &&
-          (len_seed_SE == 256) && (len_s == 256) && (len_z == 128) &&
-          (len_pkh == 256) && (len_χ == 16) && (D == 16) && (B == 4));
+  return ((n == 640) && (n̄ == 8) && (len_sec == 128) && (len_SE == 128) &&
+          (len_A == 128) && (B == 2) && (D == 15)) || // eFrodoKEM-640
+         ((n == 640) && (n̄ == 8) && (len_sec == 128) && (len_SE == 256) &&
+          (len_A == 128) && (B == 2) && (D == 15)) || // FrodoKEM-640
+         ((n == 976) && (n̄ == 8) && (len_sec == 192) && (len_SE == 192) &&
+          (len_A == 128) && (B == 3) && (D == 16)) || // eFrodoKEM-976
+         ((n == 976) && (n̄ == 8) && (len_sec == 192) && (len_SE == 384) &&
+          (len_A == 128) && (B == 3) && (D == 16)) || // FrodoKEM-976
+         ((n == 1344) && (n̄ == 8) && (len_sec == 256) && (len_SE == 256) &&
+          (len_A == 128) && (B == 4) && (D == 16)) || // eFrodoKEM-1344
+         ((n == 1344) && (n̄ == 8) && (len_sec == 256) && (len_SE == 512) &&
+          (len_A == 128) && (B == 4) && (D == 16)) // FrodoKEM-1344
+    ;
 }
 
 // Compile-time executable check for ensuring that FrodoKEM encapsulation
-// routine is invoked with proper arguments, as suggested on table 4 of FrodoKEM
-// specification.
+// routine is invoked with proper arguments, as suggested on table A.1, A.2 of
+// FrodoKEM specification.
 constexpr bool
-check_frodo_encaps_params(const size_t n,
-                          const size_t m_bar,
-                          const size_t n_bar,
-                          const size_t len_seed_A,
-                          const size_t len_seed_SE,
-                          const size_t len_ss,
-                          const size_t len_k,
-                          const size_t len_μ,
-                          const size_t len_pkh,
-                          const size_t len_χ,
-                          const size_t D,
-                          const size_t B)
+check_encaps_params(const size_t n,
+                    const size_t n̄,
+                    const size_t len_sec,
+                    const size_t len_SE,
+                    const size_t len_A,
+                    const size_t len_salt,
+                    const size_t B,
+                    const size_t D)
 {
-  return ((n == 640) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 128) && (len_ss == 128) &&
-          (len_k == 128) && (len_μ == 128) && (len_pkh == 128) &&
-          (len_χ == 16) && (D == 15) && (B == 2)) ||
-         ((n == 976) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 192) && (len_ss == 192) &&
-          (len_k == 192) && (len_μ == 192) && (len_pkh == 192) &&
-          (len_χ == 16) && (D == 16) && (B == 3)) ||
-         ((n == 1344) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 256) && (len_ss == 256) &&
-          (len_k == 256) && (len_μ == 256) && (len_pkh == 256) &&
-          (len_χ == 16) && (D == 16) && (B == 4));
+  return ((n == 640) && (n̄ == 8) && (len_sec == 128) && (len_SE == 128) &&
+          (len_A == 128) && (len_salt == 0) && (B == 2) &&
+          (D == 15)) || // eFrodoKEM-640
+         ((n == 640) && (n̄ == 8) && (len_sec == 128) && (len_SE == 256) &&
+          (len_A == 128) && (len_salt == 256) && (B == 2) &&
+          (D == 15)) || // FrodoKEM-640
+         ((n == 976) && (n̄ == 8) && (len_sec == 192) && (len_SE == 192) &&
+          (len_A == 128) && (len_salt == 0) && (B == 3) &&
+          (D == 16)) || // eFrodoKEM-976
+         ((n == 976) && (n̄ == 8) && (len_sec == 192) && (len_SE == 384) &&
+          (len_A == 128) && (len_salt == 284) && (B == 3) &&
+          (D == 16)) || // FrodoKEM-976
+         ((n == 1344) && (n̄ == 8) && (len_sec == 256) && (len_SE == 256) &&
+          (len_A == 128) && (len_salt == 0) && (B == 4) &&
+          (D == 16)) || // eFrodoKEM-1344
+         ((n == 1344) && (n̄ == 8) && (len_sec == 256) && (len_SE == 512) &&
+          (len_A == 128) && (len_salt == 512) && (B == 4) &&
+          (D == 16)) // FrodoKEM-1344
+    ;
 }
 
 // Compile-time executable check for ensuring that FrodoKEM decapsulation
-// routine is invoked with proper arguments, as suggested on table 4 of FrodoKEM
-// specification.
+// routine is invoked with proper arguments, as suggested on table A.1, A.2 of
+// FrodoKEM specification.
 constexpr bool
-check_frodo_decaps_params(const size_t n,
-                          const size_t m_bar,
-                          const size_t n_bar,
-                          const size_t len_seed_A,
-                          const size_t len_seed_SE,
-                          const size_t len_s,
-                          const size_t len_ss,
-                          const size_t len_k,
-                          const size_t len_μ,
-                          const size_t len_pkh,
-                          const size_t len_χ,
-                          const size_t D,
-                          const size_t B)
+check_decaps_params(const size_t n,
+                    const size_t n̄,
+                    const size_t len_sec,
+                    const size_t len_SE,
+                    const size_t len_A,
+                    const size_t len_salt,
+                    const size_t D,
+                    const size_t B)
 {
-  return ((n == 640) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 128) && (len_s == 128) &&
-          (len_ss == 128) && (len_k == 128) && (len_μ == 128) &&
-          (len_pkh == 128) && (len_χ == 16) && (D == 15) && (B == 2)) ||
-         ((n == 976) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 192) && (len_s == 192) &&
-          (len_ss == 192) && (len_k == 192) && (len_μ == 192) &&
-          (len_pkh == 192) && (len_χ == 16) && (D == 16) && (B == 3)) ||
-         ((n == 1344) && (m_bar == 8) && (m_bar == n_bar) &&
-          (len_seed_A == 128) && (len_seed_SE == 256) && (len_s == 256) &&
-          (len_ss == 256) && (len_k == 256) && (len_μ == 256) &&
-          (len_pkh == 256) && (len_χ == 16) && (D == 16) && (B == 4));
+  return check_encaps_params(n, n̄, len_sec, len_SE, len_A, len_salt, B, D);
 }
 
 }
