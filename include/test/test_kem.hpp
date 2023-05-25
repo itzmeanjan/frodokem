a577bde50b1b245e7760244d6398ca25e7152692
Anjan Roy <hello@itzmeanjan.in>
2023-05-25 11:31:56 +0400

    don't forget to use (not required for ephemeral case) `salt` when encapsulating
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/test/test_kem.hpp
+++ include/test/test_kem.hpp
@@ -40,6 +40,7 @@ test_kem()
   std::vector<uint8_t> pkey(pklen, 0);
   std::vector<uint8_t> skey(sklen, 0);
   std::vector<uint8_t> μ(len_sec / 8, 0);
+  std::vector<uint8_t> salt(len_salt / 8, 0);
   std::vector<uint8_t> enc(ctlen, 0);
   std::vector<uint8_t> ss0(len_sec / 8, 0);
   std::vector<uint8_t> ss1(len_sec / 8, 0);
@@ -50,6 +51,7 @@ test_kem()
   std::span<uint8_t, pklen> _pkey{ pkey };
   std::span<uint8_t, sklen> _skey{ skey };
   std::span<uint8_t, len_sec / 8> _μ{ μ };
+  std::span<uint8_t, len_salt / 8> _salt{ salt };
   std::span<uint8_t, ctlen> _enc{ enc };
   std::span<uint8_t, len_sec / 8> _ss0{ ss0 };
   std::span<uint8_t, len_sec / 8> _ss1{ ss1 };
@@ -60,11 +62,13 @@ test_kem()
   prng.read(_seedSE);
   prng.read(_z);
   prng.read(_μ);
+  prng.read(_salt);
 
   using namespace kem;
 
   keygen<n, n̄, len_sec, len_SE, len_A, B, D>(_s, _seedSE, _z, _pkey, _skey);
-  encaps<n, n̄, len_sec, len_SE, len_A, len_salt, B, D>(_μ, _pkey, _enc, _ss0);
+  encaps<n, n̄, len_sec, len_SE, len_A, len_salt, B, D>(
+    _μ, _salt, _pkey, _enc, _ss0);
   decaps<n, n̄, len_sec, len_SE, len_A, len_salt, D, B>(_skey, _enc, _ss1);
 
   assert(std::ranges::equal(ss0, ss1));
